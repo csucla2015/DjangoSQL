95cc08c7a0b66e80dbf4a62fe1437ee7affc3c64
csucla2015 <meetbhagdev@ucla.edu>
2015-04-07 16:00:04 -0700

    removed password

--- votes/votes/createTable.py
+++ votes/votes/createTable.py
@@ -1,5 +1,5 @@
 import pymssql
-conn = pymssql.connect(server='fejcz4m54q.database.windows.net', user='meet_bhagdev@fejcz4m54q', password='channelV1', database='meet_bhagdev')
+conn = pymssql.connect(server='fejcz4m54q.database.windows.net', user='meet_bhagdev@fejcz4m54q', password='*********', database='meet_bhagdev')
 cursor = conn.cursor()
 cursor.execute("""
 IF OBJECT_ID('votes', 'U') IS NOT NULL
